ed18959e26db4e8546d23bf9376b45746498c902
Anatr1 <35397698+Anatr1@users.noreply.github.com>
2021-10-24 18:07:42 +0200

    Add files via upload

---
+++ cputemp.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+z0=$(cat /sys/class/thermal/thermal_zone0/temp)
+z1=$(cat /sys/class/thermal/thermal_zone1/temp)
+z2=$(cat /sys/class/thermal/thermal_zone2/temp)
+z3=$(cat /sys/class/thermal/thermal_zone3/temp)
+z4=$(cat /sys/class/thermal/thermal_zone4/temp)
+
+z0=`expr $z0 / 1000`
+z1=`expr $z1 / 1000` #Always 20°C on my machine
+z2=`expr $z2 / 1000`
+z3=`expr $z3 / 1000`
+z4=`expr $z4 / 1000`
+av=`expr $z0 + $z2 + $z3 + $z4`
+av=`expr $av / 4 `
+
+echo "Thermal zone 0: $z0°C"
+echo "Thermal zone 1: $z1°C"
+echo "Thermal zone 2: $z2°C"
+echo "Thermal zone 3: $z3°C"
+echo "Thermal zone 4: $z4°C"
+echo "Average: $av°C"
---
+++ search_here.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+# Recursively searches for a given string in the contents of files in the given hierarchy
+
+
+if [ $# -ne 2 ]; then 
+    echo "Usage: $0 <absolute_path> <string_to_search>"
+    exit
+fi
+
+OLD_PATH=`pwd`
+NEW_PATH=$1
+STRING=$2
+
+for f in `ls $NEW_PATH` ;
+    do
+        if [[ -d "$NEW_PATH/$f" ]] 
+        then
+            $0 $NEW_PATH/$f $STRING
+        else
+            RES=`cat "$NEW_PATH/$f" 2> /dev/null | grep "$STRING" | wc -m`
+            if [ $RES -gt 0 ]
+            then
+                echo "$NEW_PATH/$f"
+            fi
+        fi
+    done
