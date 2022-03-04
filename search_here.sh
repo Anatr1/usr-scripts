4ea00bd0deeb460266b2aa265b25d4da27131cc6
Anatr1 <fedemustik@gmail.com>
2022-03-04 14:56:56 +0100

    Bug fix

--- search_here.sh
+++ search_here.sh
@@ -2,14 +2,14 @@
 # Recursively searches for a given string in the contents of files in the given hierarchy
 
 
-if [ $# -ne 2 ]; then 
+if [ $# -lt 2 ]; then
     echo "Usage: $0 <absolute_path> <string_to_search>"
     exit
 fi
 
 #OLD_PATH=`pwd`
 NEW_PATH=$1
-STRING=$2
+STRING="$2"
 
 #echo $NEW_PATH
 
@@ -17,7 +17,7 @@ for f in `ls $NEW_PATH` ;
     do
         if [[ -d "$NEW_PATH/$f" ]] 
         then
-            $0 $NEW_PATH/$f $STRING
+            $0 $NEW_PATH/$f "$STRING"
         else
             RES=`cat "$NEW_PATH/$f" 2> /dev/null | grep "$STRING" | wc -m`
             if [ $RES -gt 0 ]
